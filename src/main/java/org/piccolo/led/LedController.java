86b66c70f5088f866f9c0464fa9af554bbb87815
Siim Talvik <siim.talvik@nortal.com>
2014-10-24 17:58:32 +0300

    rgb led finished up

--- src/main/java/org/piccolo/led/LedController.java
+++ src/main/java/org/piccolo/led/LedController.java
@@ -1,12 +1,13 @@
 package org.piccolo.led;
 
 import org.piccolo.FeedbackController;
+import org.piccolo.led.impl.LedColor;
 
 /**
  * Created by mrt on 24.10.2014.
  */
 public interface LedController extends FeedbackController {
 
-    void on();
+    void on(LedColor color);
     void off();
 }
--- src/main/java/org/piccolo/led/impl/RgbLed.java
+++ src/main/java/org/piccolo/led/impl/RgbLed.java
@@ -7,33 +7,42 @@ import org.piccolo.led.LedController;
 /**
  * Created by siim on 24/10/14.
  */
-public class RgbLed implements LedController {
+public class RgbLed implements LedController{
 
-	private GpioController gpio;
-	private GpioPinDigitalOutput greenPin;
-	private GpioPinDigitalOutput redPin;
-	private GpioPinDigitalOutput bluePin;
+    private GpioController gpio;
+    private GpioPinDigitalOutput greenPin;
+    private GpioPinDigitalOutput redPin;
+    private GpioPinDigitalOutput bluePin;
 
-	public RgbLed(GpioController controller) {
-		gpio = controller;
+    public RgbLed(GpioController controller, Pin red, Pin green, Pin blue){
+        gpio = controller;
 
-		greenPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "green",
-				PinState.LOW);
-		redPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "red",
-				PinState.LOW);
-		bluePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "blue",
-				PinState.LOW);
-	}
+        greenPin = gpio.provisionDigitalOutputPin(green, "green", PinState.LOW);
+        redPin = gpio.provisionDigitalOutputPin(blue, "red", PinState.LOW);
+        bluePin = gpio.provisionDigitalOutputPin(red, "blue", PinState.LOW);
+    }
 
-	@Override
-	public void on() {
-		greenPin.setState(PinState.HIGH);
-	}
+    @Override
+    public void on(LedColor color) {
+        switch (color){
+            case blue:
+                bluePin.setState(true);
+                break;
+            case red:
+                redPin.setState(true);
+                break;
+            case green:
+                greenPin.setState(true);
+                break;
+        }
+    }
 
-	@Override
-	public void off() {
-		greenPin.setState(PinState.LOW);
-	}
+    @Override
+    public void off() {
+        greenPin.setState(false);
+        redPin.setState(false);
+        bluePin.setState(false);
+    }
 
 	@Override
 	public void onSuccess() {
