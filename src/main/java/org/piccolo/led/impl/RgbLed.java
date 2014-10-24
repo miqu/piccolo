ebe63008fc05ac5db8462f8c38bce6bf9b77faf8
Imre Lumiste <imre.lumiste@nortal.com>
2014-10-24 17:44:16 +0300

    RgbLed compiling problems fixed

--- src/main/java/org/piccolo/led/impl/RgbLed.java
+++ src/main/java/org/piccolo/led/impl/RgbLed.java
@@ -7,30 +7,44 @@ import org.piccolo.led.LedController;
 /**
  * Created by siim on 24/10/14.
  */
-public class RgbLed implements LedController{
+public class RgbLed implements LedController {
 
-    private GpioController gpio;
-    private GpioPinDigitalOutput greenPin;
-    private GpioPinDigitalOutput redPin;
-    private GpioPinDigitalOutput bluePin;
+	private GpioController gpio;
+	private GpioPinDigitalOutput greenPin;
+	private GpioPinDigitalOutput redPin;
+	private GpioPinDigitalOutput bluePin;
 
-    public RgbLed(GpioController controller, Pin red, Pin green, Pin blue){
-        gpio = controller;
+	public RgbLed(GpioController controller, Pin red, Pin green, Pin blue) {
+		gpio = controller;
 
-        greenPin = gpio.provisionDigitalOutputPin(green, "green", PinState.LOW);
-        redPin = gpio.provisionDigitalOutputPin(blue, "red", PinState.LOW);
-        bluePin = gpio.provisionDigitalOutputPin(red, "blue", PinState.LOW);
-    }
+		greenPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "green",
+				PinState.LOW);
+		redPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "red",
+				PinState.LOW);
+		bluePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "blue",
+				PinState.LOW);
+	}
 
-    @Override
-    public void on() {
-        greenPin.setState(PinState.HIGH);
-    }
+	@Override
+	public void on() {
+		greenPin.setState(PinState.HIGH);
+	}
 
-    @Override
-    public void off() {
-        greenPin.setState(PinState.LOW);
-    }
+	@Override
+	public void off() {
+		greenPin.setState(PinState.LOW);
+	}
 
+	@Override
+	public void onSuccess() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onFail() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
