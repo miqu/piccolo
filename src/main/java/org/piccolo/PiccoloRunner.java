424a2605f27198b7592f7c92a49e8458382ac432
Imre Lumiste <imre.lumiste@nortal.com>
2014-10-24 15:21:00 +0300

    added gpio.shutdown() at the end

--- src/main/java/org/piccolo/PiccoloRunner.java
+++ src/main/java/org/piccolo/PiccoloRunner.java
@@ -18,11 +18,15 @@ public class PiccoloRunner implements Runnable{
     public static void main(String ... args){
         // create gpio controller instance
         final GpioController gpio = GpioFactory.getInstance();
-        // provision gpio pins #04 as an output pin and make sure is is set to LOW at startup
-        GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04,   // PIN NUMBER
-                "My LED",           // PIN FRIENDLY NAME (optional)
-                PinState.LOW);      // PIN STARTUP STATE (optional)
-        blink(myLed);
+        try {
+	        // provision gpio pins #04 as an output pin and make sure is is set to LOW at startup
+	        GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04,   // PIN NUMBER
+	                "My LED",           // PIN FRIENDLY NAME (optional)
+	                PinState.LOW);      // PIN STARTUP STATE (optional)
+	        blink(myLed);
+        } finally {
+        	gpio.shutdown();
+        }
     }
 
 
