eccc51885fc3dffd5f1de4a64d8692cfe16e7afb
Siim Talvik <siim.talvik@nortal.com>
2014-10-24 18:56:51 +0300

    led controller is now blinking and lockcontroller initial version has been added.

---
+++ src/main/java/org/piccolo/lock/LockController.java
@@ -0,0 +1,31 @@
+package org.piccolo.lock;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+import org.piccolo.FeedbackController;
+
+/**
+ * Created by mrt on 24.10.2014.
+ */
+public class LockController implements FeedbackController {
+
+    private GpioPinDigitalOutput unlockPin;
+
+    public LockController(GpioController gpio, Pin lockPin){
+        unlockPin = gpio.provisionDigitalOutputPin(lockPin, "unlockPin", PinState.LOW);
+    }
+
+    private void unlock(long timeInMillis){
+        unlockPin.blink(timeInMillis);
+    }
+
+	public void onSuccess() {
+		unlock(5000);
+	}
+
+	public void onFail() {
+		// do nothing
+	}
+}
