c8c8007da20f1962a2e7fd8bf841ffdaeca2e14b
Siim Talvik <siim.talvik@nortal.com>
2014-10-24 19:06:37 +0300

    LockController is able to unlock

--- src/main/java/org/piccolo/lock/LockController.java
+++ src/main/java/org/piccolo/lock/LockController.java
@@ -18,7 +18,7 @@ public class LockController implements FeedbackController {
     }
 
     private void unlock(long timeInMillis){
-        unlockPin.blink(timeInMillis);
+        unlockPin.pulse(timeInMillis, PinState.HIGH);
     }
 
 	public void onSuccess() {
