49579f404852edd6252912a8eeb839dcce6992ca
miqu <mika.rastas@gmail.com>
2014-10-25 11:11:44 +0300

    Piccolo now has beeper making noise when you dont get in.

--- src/main/java/org/piccolo/ReaderController.java
+++ src/main/java/org/piccolo/ReaderController.java
@@ -25,7 +25,7 @@ public class ReaderController {
             try {
                 luettu=rc.readId();
                 if(luettu==null){
-                    Thread.sleep(100);
+                    Thread.sleep(1000);
                 }
 
             } catch (IOException e) {
